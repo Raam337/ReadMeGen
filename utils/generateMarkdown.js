55ef4cabade69b1b9c85e65ad88088a5b090066b
RobF <raam337@gmail.com>
2024-02-18 21:36:15 +0000

    markdown template complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(data) { //markdown template
   let licenseBadge = "No license selected";
   switch (data.license) { 
     case "MIT":
